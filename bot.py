d3d63d502dba0baebc0f3d6943cdfbf8bb92d4c9
leykoplasty <109410616+leykoplasty@users.noreply.github.com>
2022-12-01 21:14:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,10 +5,14 @@ import main
 token = personal_data.token
 bot = telebot.TeleBot(token)
 
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, 'Привет, чтобы я откликнулся на вакансии, введи "Хочу найти работу"')
+
 @bot.message_handler(regexp="Хочу найти работу")
 def send_report(message):
-    bot.reply_to(message, 'Привет, я буду откликаться на вакансии за тебя')
-    url_serch_form = main.parser()
-    #print(url_serch_form)
-    bot.reply_to(message, 'Откликаюсь на все вакансии по этой ссылке:'+url_serch_form)
+    bot.reply_to(message, 'Я буду откликаться на вакансии за тебя')
+    all_vacs = main.parser(bot,message)
+    main.write_gt(all_vacs)
+
 bot.infinity_polling()
