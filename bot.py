a00c7c15eb65ed7bdb3044d39ac5771d8d7d2018
leykoplasty <109410616+leykoplasty@users.noreply.github.com>
2022-12-05 07:50:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,8 @@ def send_welcome(message):
 @bot.message_handler(regexp="Хочу найти работу")
 def send_report(message):
     bot.reply_to(message, 'Я буду откликаться на вакансии за тебя')
-    all_vacs = main.parser(bot,message)
-    main.write_gt(all_vacs)
+    main.parser(bot,message)
+    bot.reply_to(message, 'Я откликнулся на 100 вакансий и записал информацию о них в таблицу по этой ссылке:https://docs.google.com/spreadsheets/d/1H8XJHqJivpWBwZleqYAPXZ31S1QgeL1eKLJirw4okgI/edit#gid=0')
+
 
 bot.infinity_polling()
